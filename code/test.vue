adef7c00919045f9d31fb475e9e6cc146f1730ac
white-shirt <775321614@qq.com>
2020-10-26 16:00:21 +0800

    Add files via upload

---
+++ code/test.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="traffic_color" style="width:50px;height:50px;"></div>
+</template>
+<script>
+export default {
+  name: 'test',
+  data() {
+    return {}
+  },
+  mounted() {
+    this.init()
+  },
+  methods: {
+    sleep(duration) {
+      return new Promise((resolve) => {
+        setTimeout(resolve, duration)
+      })
+    },
+    async changeColor(duration, color) {
+      let ele = document.getElementById('traffic_color')
+      ele.style.background = color
+      await this.sleep(duration)
+    },
+    async init() {
+      while (true) {
+        await this.changeColor(3000, 'green')
+        await this.changeColor(1000, 'yellow')
+        await this.changeColor(2000, 'red')
+      }
+    }
+  }
+}
+</script>
